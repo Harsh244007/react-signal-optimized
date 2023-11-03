c98da101ceec1c5aec2528713250b253c389df40
Harsh Patel <Patelharsh241999@gmail.com>
2023-11-03 14:03:38 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+const App=()=> {
+  
+  return (
+    <>
+  <p className="text-blue text-center text-2xl">Hello There</p>
+  <p className="text-blue text-center text-2xl">This is an template please add or remove files here.</p>
+    </>
+  )
+}
+
+export default App
